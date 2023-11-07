1768aaeff3097a8a691eb66624a38b07e2379eee
Viraj Panwar <53234568+virajpanwar@users.noreply.github.com>
2023-11-07 21:34:17 +0530

    Create day-3-start.py

---
+++ day-3-start.py
@@ -0,0 +1,11 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+    print("You can ride the rollercoaster!")
+    age=int(input("What is your age? "))
+    if age < 18:
+      print("You have to pay $7")
+    else:
+      print("You have to pay $18")
+else:
+  print("Sorry, you are too short to ride the rollercoaster.")
